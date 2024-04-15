95eca408348c58bf7b2072049a455423c07535ba
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-04-15 12:20:50 +0700

    customize prettier config and implement projectwide

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: false,
+  printWidth: 100,
   plugins: ["prettier-plugin-tailwindcss"],
-};
+}
 
-export default config;
+export default config
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,8 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
-import {
-  index,
-  pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm"
+import { index, pgTableCreator, serial, timestamp, varchar } from "drizzle-orm/pg-core"
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -16,7 +10,7 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `theo-t3-gallery_${name}`);
+export const createTable = pgTableCreator((name) => `theo-t3-gallery_${name}`)
 
 export const posts = createTable(
   "post",
@@ -30,5 +24,5 @@ export const posts = createTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
-);
+  }),
+)
